e2e8f9fe7f5ba94051bbcd90cad755bda4157f32
JustLooller <simonegiambrone2001.2@gmail.com>
2024-01-28 12:59:25 +0100

    completed minmax impl

--- Final Project/main.py
+++ Final Project/main.py
@@ -1,6 +1,8 @@
 import random
 from game import Game, Move, Player
+from tqdm import tqdm
 
+from minmax import MinMax
 
 class RandomPlayer(Player):
     def __init__(self) -> None:
@@ -12,21 +14,24 @@ class RandomPlayer(Player):
         return from_pos, move
 
 
-class MyPlayer(Player):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
-        from_pos = (random.randint(0, 4), random.randint(0, 4))
-        move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
-        return from_pos, move
-
-
 if __name__ == '__main__':
-    g = Game()
-    g.print()
-    player1 = MyPlayer()
-    player2 = RandomPlayer()
-    winner = g.play(player1, player2)
-    g.print()
-    print(f"Winner: Player {winner}")
+
+    wins = 0
+    for _ in tqdm(range(10), desc="MinMax vs Random", unit="game"):
+        g = Game()
+        player1 = MinMax(0, 2)
+        player2 = RandomPlayer()
+        winner = g.play(player1, player2)
+        if winner == 0: wins += 1
+    print(f"MinMax player with depth 2 as first player wins: {wins}")
+    print(f"Random player as second player wins: {10 - wins}")
+
+    wins = 0
+    for i in tqdm(range(10), desc="Random vs MinMax", unit="game"):
+        g = Game()
+        player1 = RandomPlayer()
+        player2 = MinMax(1, 2)
+        winner = g.play(player1, player2)
+        if winner == 1: wins += 1
+    print(f"Random player as first player wins: {10 - wins}")
+    print(f"MinMax player with depth 2 as second player wins: {wins}")
--- Final Project/minmax.py
+++ Final Project/minmax.py
@@ -1,21 +1,70 @@
 from game import Game, Player, Move
-from copy import deepcopy, copy
-from typing import Tuple
-from random import random
+from copy import deepcopy
 from math import inf
-import numpy as np
+from typing import Tuple
 from tqdm import tqdm
 
-class MinMax:
-  def __init__(self, player_id: int) -> None:
+class MinMax(Player):
+  def __init__(self, player_id: int, depth=2) -> None:
+    super().__init__()
     self.player_id = player_id
     self.opponent_id = 1 if player_id == 0 else 0
+    self.depth = depth
 
-    def evaluate(self, game: 'Game') -> int:
-      if game.check_winner() == self.player_id:
-        return 1
-      elif game.check_winner() == self.opponent_id:
-        return -1
-      else:
-        return 0
-      
+  def evaluate(self, game: 'Game') -> int:
+    if game.check_winner() == self.player_id:
+      return self.player_id
+    elif game.check_winner() == self.opponent_id:
+      return self.opponent_id
+    else:
+      return -1
+
+
+  def minimax(self, game: 'Game', depth: int, alpha: int, beta: int, maximizing_player: bool) -> int:
+    if depth == 0 or game.check_winner() != -1:
+      return self.evaluate(game)
+
+    if maximizing_player:
+      max_eval = -inf
+      possible_moves = game.possible_moves(self.player_id)
+
+      for move in possible_moves:
+        new_game = deepcopy(game)
+        new_game.move(move[0], move[1], self.player_id)
+        score = self.minimax(new_game, depth - 1, alpha, beta, False)
+        max_eval = max(max_eval, score)
+        alpha = max(alpha, score)
+        if beta <= alpha:
+          break
+      return max_eval
+    else:
+      min_eval = inf
+      possible_moves = game.possible_moves(self.opponent_id)
+
+      for move in possible_moves:
+        new_game = deepcopy(game)
+        new_game.move(move[0], move[1], self.opponent_id)
+        score = self.minimax(new_game, depth - 1, alpha, beta, True)
+        min_eval = min(min_eval, score)
+        beta = min(beta, score)
+        if beta <= alpha:
+          break
+      return min_eval   
+
+
+  def make_move(self, game: 'Game') -> Tuple[Tuple[int, int], Move]:
+    best_score = -inf
+    best_move = None
+    possible_moves = game.possible_moves(self.player_id)
+
+    for m in possible_moves:
+      new_game = deepcopy(game)
+      new_game.move(m[0], m[1], self.player_id)
+      score = self.minimax(new_game, self.depth, -inf, inf, False)
+
+      if score > best_score:
+        best_score = score
+        best_move = m
+
+    from_pos, move = best_move
+    return from_pos, move
